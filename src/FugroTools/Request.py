8e4cfc5844b151b77faaa61c44cf667f075d2fd9
Tommy <yuenkamwa@gmail.com>
2022-02-15 17:03:33 +0800

    Version 0.0.7 and upload to PIP website

--- src/FugroTools/Request.py
+++ src/FugroTools/Request.py
@@ -2,7 +2,14 @@ import requests, time
 
 def GetData(url, jsonData):
     r = requests.post(url, jsonData)
-    return r.json()
+    while True:
+        if 200 <= r.status_code <= 226:
+            return "OK", r.json(), r.status_code
+        else:
+            break
+    return "Fail", None, r.status_code
+
+
 
 def delay(milliseconds=500):
     count = int(milliseconds / 25)
